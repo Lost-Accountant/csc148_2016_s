bb4ecca6de0a0451bc6e04fc560068cf0c10a08c
Lost-Accountant <gxsgtghost141@gmail.com>
2020-08-02 23:16:36 -0400

    figure out what is happening for readline problem in load_log

--- Assignments/a2/network.py
+++ Assignments/a2/network.py
@@ -13,16 +13,101 @@ class Network(object):
     """
 
     # === Attributes ===
+    def __init__(self, name=None, asset=None, child=None):
+        """
+        Create a Network self with content value of its name, asset,
+        rank as children itself, and 0 or more children
+
+        @param name: the name of the person in the Network
+        @type name: str
+        @param asset: the asset value held by the person
+        @type asset: int
+        @param child: the children of itself, possibly an empty list
+        @type child: list[Network]
+        """
+        self.name = name
+        self.asset = asset
+        self.child = child.copy() if child else []
+
+    def __repr__(self):
+        """
+        Return representation of Network (self) as string that can be evaludated
+        into an equivalent Network
+
+        @rtype: str
+
+        >>> n1 = Network("John", 100)
+        >>> n1
+        Network('John', $100)
+        >>> n2 = Network("Smith", 150, [n1])
+        >>> n2
+        Network('Smith', $150, [Network('John', $100)])
+        """
+        return ('Network({}, ${}, {})'.format(repr(self.name),
+                                                repr(self.asset),
+                                                repr(self.child))
+                if self.child
+                else 'Network({}, ${})'.format(repr(self.name),
+                                                repr(self.asset)))
+
+    def __contains__(self, name):
+        """
+        Return whether Network self contains name.
+
+        @param name: a certain name to search for
+        @type name: str
+        @rtype: bool
+
+        >>> n1 = Network("John", 100)
+        >>> n1.__contains__('John')
+        True
+        >>> n2 = Network("Smith", 150, [n1])
+        >>> n2.__contains__('John')
+        True
+        >>> n2.__contains__('Charles')
+        False
+        """
+        if len(self.child) == 0:
+            return self.name == name
+        else:
+            return self.name == name or any([name in each for each in self.child])
     # TODO: Complete this part
 
     def load_log(self, log_file_name):
-        """Load the network topology from the log log_file_name.
-
-        TODO: Complete this part
         """
+        Load the network topology from the log log_file_name.
 
-        #TODO: Complete this part
+        @param self: Network self
+        @type self: Network
+        @param log_file_name: the name of the file for network topology
+        @type log_file_name: str
 
+        >>> network = Network()
+        >>> network.load_log("topology0.txt")
+        >>> network
+        Network('Liam', $20, [Network('Emma', $32, [Network('Mason', $14)])])
+        """
+        open_file = open(log_file_name, 'r')
+        for line in open_file:
+            info = line.split('#')
+            if len(info) == 2: # boss slot, no parent
+                self.name = info[0]
+                self.asset = info[1]
+            else:
+                # has parent
+                # parent already in
+                if self.__contains__(info[1]):
+                    current = self
+                    # search for parent
+                    while current.name != info[1]:
+                        # move down if that branch contains name
+                        for each in current.child.copy():
+                            if each.__contains__(info[1]):
+                                current = each
+                                # create child from parent
+                    current.child.append(Network(name=info[0],asset=info[1]))
+
+        open_file.close()
 
     def sponsor(self, member_name):
         """Return the sponsor name of member with name member_name.
